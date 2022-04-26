3c8fe8e0a7f1e3d0ed0e6028c66331980bd39384
Morgan <morgan35543@gmail.com>
2022-04-26 12:05:48 +0100

    Undone debug true

--- yata/settings.py
+++ yata/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 print(f"[YATA {datestr()}] settings DEBUG={DEBUG}")
 
 # SECURITY WARNING: keep the secret key used in production secret!
